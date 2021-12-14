b62d78f69ef8e213706160693f64af967af75a37
Ohad Shai <oshai@google.com>
2021-12-14 10:58:38 +0200

    first example

--- src/main/kotlin/io/github/oshai/kf/KotlinFlogger.kt
+++ src/main/kotlin/io/github/oshai/kf/KotlinFlogger.kt
@@ -1,7 +1,70 @@
 package io.github.oshai.kf
 
-fun main() {
-    println("hi")
+import com.google.common.flogger.AbstractLogger
+import com.google.common.flogger.LogContext
+import com.google.common.flogger.LogSite
+import com.google.common.flogger.LoggingApi
+import com.google.common.flogger.backend.Platform
+
+
+// class KotlinFlogger {}
+
+fun <API : LoggingApi<API>> AbstractLogger<API>.atInfo(f: LoggingApi<API>.() -> Unit) {
+  this.atInfo()
+    .withInjectedLogSite(LazyKotlinLogSite())
+    .apply(f)
 }
-class KotlinFlogger {
+
+var <API : LoggingApi<API>> LoggingApi<API>.cause: Throwable
+  get() = TODO()
+  set(value) {
+    withCause(value)
+  }
+
+var <API : LoggingApi<API>> LoggingApi<API>.every: Int
+  get() = TODO()
+  set(value) {
+    every(value)
+  }
+
+
+private class LazyKotlinLogSite : LogSite() {
+
+  private val internalLogSite: LogSite by lazy {
+    Platform.getCallerFinder().findLogSite(
+      LogContext::class.java, 1)
+  }
+
+  private val classNameKt: String by lazy {
+    // io.github.oshai.kf.FloggerTestKt$myKMain$1
+    // another option is to use internalLogSite.fileName
+    val classNameLogSite = internalLogSite.className.substringBefore('$')
+    if (classNameLogSite.endsWith("Kt")) {
+      classNameLogSite.substring(0, classNameLogSite.length - 2)
+    } else {
+      classNameLogSite
+    }
+  }
+
+  private val methodNameKt: String by lazy {
+    // io.github.oshai.kf.FloggerTestKt$myKMain$1
+    internalLogSite.className.substringBeforeLast('$').substringAfterLast('$')
+  }
+
+  override fun getClassName(): String {
+    return classNameKt
+  }
+
+  override fun getMethodName(): String {
+    return methodNameKt
+  }
+
+  override fun getLineNumber(): Int {
+    return internalLogSite.lineNumber
+  }
+
+  override fun getFileName(): String? {
+    return internalLogSite.fileName
+  }
+
 }
--- src/test/kotlin/io/github/oshai/kf/FloggerTest.kt
+++ src/test/kotlin/io/github/oshai/kf/FloggerTest.kt
@@ -1,20 +1,36 @@
 package io.github.oshai.kf
 
 import com.google.common.flogger.FluentLogger
-import com.google.common.flogger.LogSites
 import org.junit.jupiter.api.Test
 
 
 private val logger: FluentLogger = FluentLogger.forEnclosingClass()
 
+fun myKMain() {
+  logger.atInfo { log("log myKMain message") }
+}
+
+
 class FloggerTest {
 
   @Test
-  fun `test flogger`() {
-    logger.atInfo().log("Log1 message with: %s", "argument")
-    logger.atInfo().withInjectedLogSite(
-      LogSites.callerOf(KotlinFlogger::class.java)).log("Log2 message with: %s", "argument")
-    logger.atInfo().withCause(Exception()).log("Log3 message with: %s", "argument")
-    println("DONE")
+  fun `test kotlin flogger - one log file function`() {
+    myKMain()
   }
+
+  @Test
+  fun `test kotlin flogger - one log class function`() {
+    logger.atInfo { log("log class function message") }
+  }
+
+  @Test
+  fun `test kotlin flogger - properties`() {
+    logger.atInfo {
+      cause = Exception()
+      every = 5
+      log("log class function message")
+    }
+  }
+
+
 }
